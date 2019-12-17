c24c497dbca2a9e8521eea7bb0273a770266022c
Rimantas <belauzas@yahoo.com>
2019-12-17 16:26:21 +0200

    Create demo data set #2

---
+++ js/data.js
@@ -0,0 +1,132 @@
+"use strict";
+
+const posts = [
+    // {
+    //     author: {
+    //         name: 'John',
+    //         surname: 'Dear',
+    //         img: 'john-dear.jpg',
+    //         link: '#'
+    //     },
+    //     time: 1559846846,
+    //     content: {
+    //         text: 'Lorem ipsum dolor sit amet...',
+    //         background: 'red',
+    //         img: ['img-1.jpg', 'img-2.png', 'img-3.jpg']
+    //     }
+    // },
+    {
+        author: {
+            name: 'John',
+            surname: 'Dear',
+            img: 'john-dear.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis.'
+        }
+    },
+    {
+        author: {
+            name: 'John',
+            surname: 'Dear',
+            img: 'john-dear.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis.'
+        }
+    },
+    {
+        author: {
+            name: 'John',
+            surname: 'Dear',
+            img: 'john-dear.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img: ['img-1.jpg']
+        }
+    },
+    {
+        author: {
+            name: 'John',
+            surname: 'Dear',
+            img: 'john-dear.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img: ['img-1.jpg', 'img-2.jpg']
+        }
+    },
+    {
+        author: {
+            name: 'John',
+            surname: 'Dear',
+            img: 'john-dear.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img: ['img-1.jpg', 'img-2.jpg', 'img-3.jpg']
+        }
+    },
+    {
+        author: {
+            name: 'John',
+            surname: 'Dear',
+            img: 'john-dear.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img: ['img-1.jpg', 'img-2.jpg', 'img-3.jpg', 'img-4.jpg']
+        }
+    },
+    {
+        author: {
+            name: 'John',
+            surname: 'Dear',
+            img: 'john-dear.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img: ['img-1.jpg', 'img-2.jpg', 'img-3.jpg', 'img-4.jpg', 'img-2.jpg', 'img-3.jpg', 'img-4.jpg']
+        }
+    },
+    {
+        author: {
+            name: 'John',
+            surname: 'Dear',
+            img: 'john-dear.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        }
+    },
+    {
+        author: {
+            name: 'John',
+            surname: 'Dear',
+            img: 'john-dear.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            background: 'red'
+        }
+    }
+];
