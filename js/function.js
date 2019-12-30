976ec98d4ae279671ffec4f4f743979dc33af367
Rimantas <belauzas@yahoo.com>
2019-12-30 15:52:38 +0200

    Final update

--- js/function.js
+++ js/function.js
@@ -7,8 +7,8 @@ function getPosts( callback ) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            // Typical action to be performed when the document is ready:
-            callback(xhttp.responseText);
+            // gavus atsakyma i uzklausa, duomenis perduodame callback'ui
+            callback(JSON.parse(xhttp.responseText));
         }
     };
     xhttp.open("GET", API, true);
